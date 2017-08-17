f60ee0dd407018cfdffa8bb04cdb2b5eeff999af
azu <azuciao@gmail.com>
2017-08-17 20:32:22 +0900

    feat(lib): support JSON

--- test/check-invalid-control-characters-test.js
+++ test/check-invalid-control-characters-test.js
@@ -4,6 +4,10 @@ const assert = require("assert");
 const path = require("path");
 const { validateInvalidControlCharacters } = require("../lib/check-invalid-control-characters");
 describe("check-invalid-control-characters", () => {
+    it("should pass ", () => {
+        const error = validateInvalidControlCharacters(path.join(__dirname, "fixtures/pass.txt"));
+        assert.ok(error === undefined);
+    });
     it("should report error if include invalid characters", () => {
         const error = validateInvalidControlCharacters(path.join(__dirname, "fixtures/test.txt"));
         assert.ok(error instanceof Error, "should be reported error");
